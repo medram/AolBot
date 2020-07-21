b3ad0c60f8bea245d4903b2532adba9e6e67bfe9
medram <mramouchy@gmail.com>
2020-07-21 16:37:24 +0100

    updated action 'inbox_open_plus_click_messages'

--- app/actions/inbox_open_messages.py
+++ app/actions/inbox_open_messages.py
@@ -28,7 +28,7 @@ class Inbox_open_messages(ActionAbstract):
 
 		# select navbar items.
 		navbar = driver.find_elements_by_css_selector('div.navItem')
-		# click on Spam section
+		# click on Inbox section
 		navbar[1].click()
 		time.sleep(2)
 
@@ -41,38 +41,41 @@ class Inbox_open_messages(ActionAbstract):
 		actions = ActionChains(driver)
 		# Archive all messages.
 		try:
-			# clicking the first messages.
-			driver.execute_script("""
-				let messages_row = document.querySelectorAll("div.dojoxGrid-content div.dojoxGrid-row")
-				messages_row[0].classList.add("dojoxGrid-row-over")
-			""")
-			ActionChains(driver).send_keys(Keys.ENTER).perform()
+			if total_messages == 0:
+				logger.warning(f'({self.ACTION.name}) Maybe no messages are found in spam section of ({profile.email})')
+			else:
+				# clicking the first messages.
+				driver.execute_script("""
+					let messages_row = document.querySelectorAll("div.dojoxGrid-content div.dojoxGrid-row")
+					messages_row[0].classList.add("dojoxGrid-row-over")
+				""")
+				ActionChains(driver).send_keys(Keys.ENTER).perform()
 
-			# get the amount of messages to open.
-			last_message = common.get_amount_of_message(total_messages)
-			click.secho(f'({profile.email}) Total messages {total_messages}: {last_message} messages will be openned.', fg='bright_black')
+				# get the amount of messages to open.
+				last_message = common.get_amount_of_message(total_messages)
+				click.secho(f'({profile.email}) Total messages {total_messages}: {last_message} messages will be openned.', fg='bright_black')
 
-			with click.progressbar(length=last_message, label=f'Openning messages ({profile.email})...', show_pos=True) as bar:
-				for i in range(last_message):
-					actions = ActionChains(driver)
-					actions.send_keys('n')
-					# add start to the current message.
-					if random.random() <= app_settings.MESSAGES_STARTS_RATIO:
-						try:
-							button_flag = driver.find_element_by_css_selector('span.flag')
-							button_flag.click()
-						except Exception:
-							logger.warning('Cannot add Flags to messages, it may need a fix!')
-					actions.perform()
+				with click.progressbar(length=last_message, label=f'Openning messages ({profile.email})...', show_pos=True) as bar:
+					for i in range(last_message):
+						actions = ActionChains(driver)
+						actions.send_keys('n')
+						# add start to the current message.
+						if random.random() <= app_settings.MESSAGES_STARTS_RATIO:
+							try:
+								button_flag = driver.find_element_by_css_selector('span.flag')
+								button_flag.click()
+							except Exception:
+								logger.warning('Cannot add Flags to messages, it may need a fix!')
+						actions.perform()
 
-					# show the progress
-					# print(f'\r{i+1}/{last_message}', end='')
-					bar.update(1) # +=1 each time
+						# show the progress
+						# print(f'\r{i+1}/{last_message}', end='')
+						bar.update(1) # +=1 each time
 
-					# clear the all chained actions (is not working, it's a bug in selenium source code).
-					# actions.reset_actions()
+						# clear the all chained actions (is not working, it's a bug in selenium source code).
+						# actions.reset_actions()
 
-					time.sleep(random.uniform(3, 5))
+						time.sleep(random.uniform(3, 5))
 
 
 		except TimeoutException:
